a816b674741dbc8760da2edda71f6fc2d287a4e5
David Hou <david.hou314@gmail.com>
2013-04-18 16:43:36 -0700

    Don't need this anymore...

--- full_crespo.mk
+++ full_crespo.mk
@@ -29,6 +29,8 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
+#PRODUCT_COPY_FILES := device/samsung/crespo/99mount.txt:system/etc/init.d/99mount.txt
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
